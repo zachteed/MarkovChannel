2a7d972f632af30562071909dc14624bed374c28
Zach Teed <zachteed@wustl.edu>
2016-05-11 23:18:16 -0500

    Create math_functions.hpp

---
+++ include/math_functions.hpp
@@ -0,0 +1,108 @@
+#ifndef MATH_FUNCTIONS_HPP_
+#define MATH_FUNCTIONS_HPP_
+
+#ifdef USE_MKL
+
+#include <mkl.h>
+
+#else
+
+extern "C" {
+  #include <cblas.h>
+}
+
+#include <math.h>
+
+#define DEFINE_VSL_UNARY_FUNC(name, operation) \
+  template<typename Dtype> \
+  void v##name(const int n, const Dtype* a, Dtype* y) { \
+    CHECK_GT(n, 0); CHECK(a); CHECK(y); \
+    for (int i = 0; i < n; ++i) { operation; } \
+  } \
+  inline void vs##name( \
+    const int n, const float* a, float* y) { \
+    v##name<float>(n, a, y); \
+  } \
+  inline void vd##name( \
+      const int n, const double* a, double* y) { \
+    v##name<double>(n, a, y); \
+  }
+
+DEFINE_VSL_UNARY_FUNC(Sqr, y[i] = a[i] * a[i]);
+DEFINE_VSL_UNARY_FUNC(Exp, y[i] = exp(a[i]));
+DEFINE_VSL_UNARY_FUNC(Ln, y[i] = log(a[i]));
+DEFINE_VSL_UNARY_FUNC(Abs, y[i] = fabs(a[i]));
+
+// A simple way to define the vsl unary functions with singular parameter b.
+// The operation should be in the form e.g. y[i] = pow(a[i], b)
+#define DEFINE_VSL_UNARY_FUNC_WITH_PARAM(name, operation) \
+  template<typename Dtype> \
+  void v##name(const int n, const Dtype* a, const Dtype b, Dtype* y) { \
+    CHECK_GT(n, 0); CHECK(a); CHECK(y); \
+    for (int i = 0; i < n; ++i) { operation; } \
+  } \
+  inline void vs##name( \
+    const int n, const float* a, const float b, float* y) { \
+    v##name<float>(n, a, b, y); \
+  } \
+  inline void vd##name( \
+      const int n, const double* a, const float b, double* y) { \
+    v##name<double>(n, a, b, y); \
+  }
+
+DEFINE_VSL_UNARY_FUNC_WITH_PARAM(Powx, y[i] = pow(a[i], b));
+
+// A simple way to define the vsl binary functions. The operation should
+// be in the form e.g. y[i] = a[i] + b[i]
+#define DEFINE_VSL_BINARY_FUNC(name, operation) \
+  template<typename Dtype> \
+  void v##name(const int n, const Dtype* a, const Dtype* b, Dtype* y) { \
+    CHECK_GT(n, 0); CHECK(a); CHECK(b); CHECK(y); \
+    for (int i = 0; i < n; ++i) { operation; } \
+  } \
+  inline void vs##name( \
+    const int n, const float* a, const float* b, float* y) { \
+    v##name<float>(n, a, b, y); \
+  } \
+  inline void vd##name( \
+      const int n, const double* a, const double* b, double* y) { \
+    v##name<double>(n, a, b, y); \
+  }
+
+DEFINE_VSL_BINARY_FUNC(Add, y[i] = a[i] + b[i]);
+DEFINE_VSL_BINARY_FUNC(Sub, y[i] = a[i] - b[i]);
+DEFINE_VSL_BINARY_FUNC(Mul, y[i] = a[i] * b[i]);
+DEFINE_VSL_BINARY_FUNC(Div, y[i] = a[i] / b[i]);
+
+
+#endif
+
+#include <stdint.h>
+#include <cmath>
+
+template <typename Dtype>
+void cpu_gemm(const CBLAS_TRANSPOSE TransA,
+  const CBLAS_TRANSPOSE TransB, const int M, const int N, const int K,
+  const Dtype alpha, const Dtype* A, const Dtype* B, const Dtype beta,
+  Dtype* C);
+
+template <typename Dtype>
+void cpu_gemv(const CBLAS_TRANSPOSE TransA, const int M, const int N,
+  const Dtype alpha, const Dtype* A, const Dtype* x, const Dtype beta,
+  Dtype* y);
+  
+template <typename Dtype>
+void cpu_exp(const int n, const Dtype* a, Dtype* y);
+
+template <typename Dtype>
+void cpu_log(const int n, const Dtype* a, Dtype* y);
+
+template <typename Dtype>
+void cpu_abs(const int n, const Dtype* a, Dtype* y);
+
+template <typename Dtype>
+Dtype cpu_dot(const int n, const Dtype* x, const Dtype* y);
+  
+
+#endif
+
