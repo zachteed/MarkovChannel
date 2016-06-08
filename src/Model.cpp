9beba4039336c3ca1772851edf6a6ef686af1337
zachteed <zachteed@wustl.edu>
2016-06-07 21:00:57 -0500

    added state mutating neighbor function

--- src/Model.cpp
+++ src/Model.cpp
@@ -75,23 +75,134 @@ namespace Model {
 
   Model* neighbor(Model* m, int num) {
 
+  //   Model* n = new Model();
+  //   n->G = m->G;
+  //
+  //   int N=n->G.N, E=n->G.E, P=prms.n_prms();
+  //   n->rs = (double*) malloc(P*(N+E)*sizeof(double));
+  //   memcpy(n->rs, m->rs, P*(N+E)*sizeof(double));
+  //   n->rk = n->rs + P*N; n->r_vec = NULL;
+  //
+  //   n->C = (double*) calloc(N, sizeof(double));
+  //   n->F = (double*) calloc(N, sizeof(double));
+  //   memcpy(n->C, m->C, N*sizeof(double));
+  //   memcpy(n->F, m->F, N*sizeof(double));
+  //
+  //   n->C[0] = 1; n->F[0] = 1;
+  //
+  //   const MarkovChannel::MutationParameter& mut = prms.mutation();
+  //   double prob=mut.update_prob(), sig=mut.update_std();
+  //
+  //   double* mult = (double*) malloc(P*(N+E)*sizeof(double));
+  //   double* r = (double*) malloc(P*(N+E)*sizeof(double));
+  //
+  //   Math::rng_uniform(P*(N+E), mult);
+  //   Math::rng_gaussian(P*(N+E), r, 0, sig);
+  //
+  //   for(int i=0; i<P*(N+E); i++) {
+  //     r[i] = (mult[i] < prob) ? r[i] : 0;
+  //     n->rs[i] += r[i];
+  //   }
+  //
+  //   Math::rng_uniform(N, mult);
+  //   Math::rng_gaussian(N, r, 0, 0.1);
+  //
+  //   for (int i=1; i<N; i++) {
+  //     if (mult[i] < 0.05) {
+  //       n->C[i] += r[i];
+  //       if (n->C[i] < 0) n->C[i] = 0;
+  //       if (n->C[i] > 1) n->C[i] = 1;
+  //     }
+  //   }
+  //
+  //   free(mult); free(r); return n;
+  // }
+
     Model* n = new Model();
-    n->G = m->G;
+    n->G = m->G; n->r_vec = NULL;
+
+    std::vector<int> node_idx;
+    std::vector<int> edge_idx;
+    int *nidx, *eidx;
+
+    for (int i=0; i<n->G.N; i++)
+      node_idx.push_back(i);
+
+    for (int i=0; i<n->G.E; i++)
+      edge_idx.push_back(i);
+
+
+    double rng[4]; Math::rng_uniform(4, rng);
+    const MarkovChannel::MutationParameter& mut = prms.mutation();
+    double prob=mut.update_prob(), sig=mut.update_std();
+
+    if (mut.has_add_edge() && rng[0] < mut.add_edge()) {
+      Graph::add_edge(n->G, false);
+
+      while(edge_idx.size() < n->G.E)
+        edge_idx.push_back(-1);
+    }
+
+    if (n->G.N < 10 && mut.has_add_node() && rng[1] < mut.add_node()) {
+      Graph::add_node(n->G, false);
+
+      while(edge_idx.size() < n->G.E)
+        edge_idx.push_back(-1);
+
+      while(node_idx.size() < n->G.N)
+        node_idx.push_back(-1);
+    }
+
+    if (mut.has_rm_edge() && rng[2] < mut.rm_edge()) {
+
+      nidx = &node_idx[0];
+      eidx = &edge_idx[0];
+      Graph::rm_edge(n->G, eidx, true);
+
+      while(edge_idx.size() < n->G.E)
+        edge_idx.push_back(-1);
+
+      while(node_idx.size() < n->G.N)
+        node_idx.push_back(-1);
+    }
+
+    if (n->G.N > 3 && mut.has_rm_node() && rng[3] < mut.rm_node()) {
+
+      nidx = &node_idx[0];
+      eidx = &edge_idx[0];
+      Graph::rm_node(n->G, nidx, eidx, true);
+
+      while(edge_idx.size() < n->G.E)
+        edge_idx.push_back(-1);
+
+      while(node_idx.size() < n->G.N)
+        node_idx.push_back(-1);
+    }
 
     int N=n->G.N, E=n->G.E, P=prms.n_prms();
     n->rs = (double*) malloc(P*(N+E)*sizeof(double));
-    memcpy(n->rs, m->rs, P*(N+E)*sizeof(double));
-    n->rk = n->rs + P*N; n->r_vec = NULL;
 
     n->C = (double*) calloc(N, sizeof(double));
     n->F = (double*) calloc(N, sizeof(double));
-    memcpy(n->C, m->C, N*sizeof(double));
-    memcpy(n->F, m->F, N*sizeof(double));
+    n->C[0] = 1.0; n->F[0] = 1; n->rk = n->rs + P*N;
 
-    n->C[0] = 1; n->F[0] = 1;
+    Math::rng_gaussian((N+E)*P, n->rs, prms.mu(), prms.std());
+    nidx = &node_idx[0];
+    eidx = &edge_idx[0];
 
-    const MarkovChannel::MutationParameter& mut = prms.mutation();
-    double prob=mut.update_prob(), sig=mut.update_std();
+
+    for (int i=0; i<N; i++) {
+      if (nidx[i] >= 0) {
+        memcpy(&n->rs[P*i], &m->rs[P*nidx[i]], P*sizeof(double));
+        n->C[i] = m->C[nidx[i]];
+        n->F[i] = m->F[nidx[i]];
+      }
+    }
+    for (int i=0; i<E; i++) {
+      if (eidx[i] >= 0) {
+        memcpy(&n->rk[P*i], &m->rk[P*eidx[i]], P*sizeof(double));
+      }
+    }
 
     double* mult = (double*) malloc(P*(N+E)*sizeof(double));
     double* r = (double*) malloc(P*(N+E)*sizeof(double));
@@ -115,132 +226,11 @@ namespace Model {
       }
     }
 
+    n->id = Model::Model::count++;
     free(mult); free(r); return n;
 
-
-    //
-    //
-    //
-    //
-    // Model* n = new Model();
-    // n->G = m.G;
-    //
-    // std::vector<int> node_idx;
-    // std::vector<int> edge_idx;
-    // int *nidx, *eidx;
-    //
-    // for (int i=0; i<n->G.N; i++)
-    //   node_idx.push_back(i);
-    //
-    // for (int i=0; i<n->G.E; i++) {
-    //   edge_idx.push_back(i);
-    // }
-    //
-    // double rng[4];
-    // Math::rng_uniform(4, rng);
-    //
-    // const MarkovChannel::MutationParameter& mut = prms.mutation();
-
-    // if (mut.has_add_edge() && rng[0] < mut.add_edge()) {
-    //   Graph::add_edge(n->G, false);
-    //
-    //   while(edge_idx.size() < n->G.E)
-    //     edge_idx.push_back(-1);
-    // }
-    //
-    // if (m.G.N < 10 && mut.has_add_node() && rng[1] < mut.add_node()) {
-    //   Graph::add_node(n->G, false);
-    //
-    //   while(edge_idx.size() < n->G.E)
-    //     edge_idx.push_back(-1);
-    //
-    //   while(node_idx.size() < n->G.N)
-    //     node_idx.push_back(-1);
-    // }
-    //
-    // if (mut.has_rm_edge() && rng[2] < mut.rm_edge()) {
-    //
-    //   nidx = &node_idx[0];
-    //   eidx = &edge_idx[0];
-    //   Graph::rm_edge(n->G, eidx, true);
-    //
-    //   while(edge_idx.size() < n->G.E)
-    //     edge_idx.push_back(-1);
-    //
-    //   while(node_idx.size() < n->G.N)
-    //     node_idx.push_back(-1);
-    // }
-    //
-    // if (m.G.N > 3 && mut.has_rm_node() && rng[3] < mut.rm_node()) {
-    //
-    //   nidx = &node_idx[0];
-    //   eidx = &edge_idx[0];
-    //   Graph::rm_node(n->G, nidx, eidx, true);
-    //
-    //   while(edge_idx.size() < n->G.E)
-    //     edge_idx.push_back(-1);
-    //
-    //   while(node_idx.size() < n->G.N)
-    //     node_idx.push_back(-1);
-    // }
-
-    // int N = n->G.N, E = n->G.E;
-    // int P = prms.n_prms();
-    //
-    // n->rs = (double*) malloc(P*(N+E)*sizeof(double));
-    // n->C = (double*) calloc(N, sizeof(double));
-    // n->F = (double*) calloc(N, sizeof(double));
-    // n->rk = n->rs + P*N;
-    //
-    // memcpy(n->rs, m.rs, P*(N+E)*sizeof(double));
-    // n->C[0] = 1;
-    // n->F[0] = 1;
-    // memcpy(n->C, m.C, N*sizeof(double));
-    // memcpy(n->F, m.F, N*sizeof(double));
-
-    // Math::rng_gaussian((N+E)*P, n->rs, prms.mu(), prms.std());
-    //
-    // nidx = &node_idx[0];
-    // eidx = &edge_idx[0];
-
-
-
-    // for (int i=0; i<N; i++) {
-    //   if (nidx[i] >= 0) {
-    //     memcpy(&n->rs[P*i], &m.rs[P*nidx[i]], P*sizeof(double));
-    //     n->C[i] = m.C[nidx[i]];
-    //     n->F[i] = m.F[nidx[i]];
-    //   }
-    // }
-    // for (int i=0; i<E; i++) {
-    //   if (eidx[i] >= 0) {
-    //     memcpy(&n->rk[P*i], &m.rk[P*eidx[i]], P*sizeof(double));
-    //   }
-    // }
-    //
-    // double* mult = (double*) malloc(P*(N+E)*sizeof(double));
-    // double* r = (double*) malloc(P*(N+E)*sizeof(double));
-    //
-    // double prob = mut.update_prob();
-    // double sig = mut.update_std();
-    //
-    // Math::rng_uniform(P*(N+E), mult);
-    // Math::rng_gaussian(P*(N+E), r, 0, sig);
-    //
-    // for(int i=0; i<P*(N+E); i++) {
-    //   r[i] = (mult[i] < prob) ? r[i] : 0;
-    //   n->rs[i] += r[i];
-    // }
-    //
-    // // // vdAdd(N+E, r, n->rs, n->rs);
-    // // for (int i=0; i<N+E; i++) n->rs[i] += r[i];
-    //
-    // n->id = Model::Model::count++;
-    // n->r_vec = NULL;
-    //
-    // free(mult); free(r);
-    // return n;
   }
+
 
   double* initial_state(Model& m, double vm, double* s)
   {
